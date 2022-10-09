16af00ee1a1b57fb8f72c688be39425764e0bb10
Takarada0202 <joun0925lim@gmail.com>
2022-10-10 00:34:33 +0900

    준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -14,9 +14,10 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member(200L, "member200");
-            em.persist(member);
-            em.flush();
+            Member member = em.find(Member.class,105L);
+            member.setName("AAAAAAA");
+            em.clear();
+            Member member2 = em.find(Member.class,105L);
 
             tx.commit();
         } catch (Exception e) {
