02616bac0647e9b2fd282e4d505017684d1e1bdf
Takarada0202 <joun0925lim@gmail.com>
2022-10-12 00:26:05 +0900

    양방향연관관계와 연관관계의 주인 1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,8 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
-    public class JpaMain {
+public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
@@ -20,8 +21,6 @@ import javax.persistence.Persistence;
             Member member = new Member();
             member.setUsername("member1");
             member.setTeam(team);
-
-            member.setTeam(team);
             em.persist(member);
 
             em.flush();
@@ -29,6 +28,11 @@ import javax.persistence.Persistence;
 
             Member findMember = em.find(Member.class, member.getId());
             Team finTeam = findMember.getTeam();
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println(m.getUsername());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
