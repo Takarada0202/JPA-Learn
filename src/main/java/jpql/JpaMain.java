8e84af313b9d65c1b01e88fb8fdff90c5d4c8fec
Takarada0202 <joun0925lim@gmail.com>
2022-11-02 02:22:36 +0900

    벌크연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,35 +15,36 @@ public class JpaMain {
             Team teamA= new Team();
             teamA.setName("팀A");
             em.persist(teamA);
+
             Team teamB= new Team();
             teamB.setName("팀B");
             em.persist(teamB);
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
             member2.setTeam(teamA);
+            member2.setAge(0);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
             member3.setTeam(teamB);
+            member3.setAge(0);
             em.persist(member3);
 
-
-
-            em.flush();
+            int resultCount = em.createQuery("update Member m set m.age = 20").executeUpdate();
             em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class).setParameter("username", "회원1").getResultList();
+            Member finMember = em.find(Member.class,member1.getId());
+            System.out.println("findMember=" +finMember.getAge());
 
-            for (Member member : resultList) {
-                System.out.println("member=" +member);
-            }
+
 
 
 
